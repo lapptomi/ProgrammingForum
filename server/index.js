fd097db52fb144daa35e15b665eea546db6d1d1f
lapptomi <lappetelainen.tomi@gmail.com>
2021-05-06 13:10:24 +0300

    test

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const http = require('http');
 const app = require('./src/app');
 
@@ -5,6 +6,5 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Server running on ${PORT}`);
 });
